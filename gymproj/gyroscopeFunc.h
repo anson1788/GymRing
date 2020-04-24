66523747f8a13a965cef60f455b72dfde8c27e1e
anson1788 <anson1788@gmail.com>
2020-04-25 01:32:30 +0800

    stable version for adL321

--- gymproj/gyroscopeFunc.h
+++ gymproj/gyroscopeFunc.h
@@ -15,9 +15,10 @@ void MPU6050_Init(){
 
 long crtClockForGyroscope = 0; 
 long lastClockForGyroscope = 0; 
+long lastClockForG = 0;
 
 void GetGyroscopeData(){
-  float AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+  double AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
   crtClockForGyroscope = millis();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);
@@ -31,36 +32,104 @@ void GetGyroscopeData(){
   GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-  //if(crtClockForGyroscope-lastClockForGyroscope >1000){
-     lastClockForGyroscope = crtClockForGyroscope;
-      
+  if(crtClockForGyroscope-lastClockForGyroscope >1000){
+
+      /*
       Serial.print("AcX = "); 
       Serial.print(AcX);
       Serial.print("||"); 
+      */
       AcX = AcX;
       if(AcX>50000){
         AcX = (AcX - 57600)/(65400-57600) -1;
       }else{
         AcX = (AcX)/(8500);
-        AcX = min(1.0f,AcX);
+        AcX = min(1.0,AcX);
       }
       AcX = - AcX;
-
-      
+      if(AcX < -1){
+        AcX = -1;
+      }
+      if(AcX > 1){
+        AcX = 1;
+      }
       /*
-      Serial.print(" | AcY = "); Serial.print(AcY);
-      Serial.print(" | AcZ = "); Serial.print(AcZ);
-      Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53); //equation for temperature in degrees C from datasheet
-      Serial.print(" | GyX = "); Serial.print(GyX);
-      Serial.print(" | GyY = "); Serial.print(GyY);
-      Serial.print(" | GyZ = "); Serial.println(GyZ);
-      */
       Serial.print(AcX);
       Serial.print("\n");
-     
-      delay(1000);
-  //}
+      */
 
+      /*
+      Serial.print("AcY = "); 
+      Serial.print(AcY);
+      Serial.print("||"); */
+      AcY = AcY;
+
+      if(AcY>50000){
+        AcY = (AcY - 57300)/(65000-57300) -1;
+      }else{
+        AcY = (AcY)/(8300);
+      }
+      AcY = - AcY;
+      if(AcY < -1){
+        AcY = -1;
+      }
+      if(AcY > 1){
+        AcY = 1;
+      }
+      /*
+      Serial.print(AcY);
+      Serial.print("\n");
+      */
+      
+      /*
+      Serial.print("AcZ = "); 
+      Serial.print(AcZ);
+      Serial.print("||"); 
+      */
+      AcZ = AcZ;
+      if(AcZ>50000){
+        AcZ = (AcZ - 57200)/(64000-57200) -1;
+      }else{
+        AcZ = (AcZ)/(8300);
+      }
+      AcZ = - AcZ;
+      if(AcZ < -1){
+        AcZ = -1;
+      }
+      if(AcZ > 1){
+        AcZ = 1;
+      }
+      /*
+      Serial.print(AcZ);
+      Serial.print("\n");
+      */
+
+      /*
+      Serial.print("GyZ = "); 
+      Serial.print(GyZ);
+      Serial.print("||"); 
+      GyroZ = (max(0.0,GyroZ-550.0) / 60477.0)*(crtClockForGyroscope-lastClockForGyroscope)/1000;
+      Serial.print("GyroZ= "); 
+      Serial.print(GyroZ);
+      Serial.print("\n");
+      */
+      Serial.print("ping\n"); 
+      lastClockForGyroscope = crtClockForGyroscope;
+  }
+
+      if(lastClockForG==0){
+         lastClockForG = crtClockForGyroscope;
+      }
+      if(GyZ>64400){
+        Serial.print("GyZ = "); 
+        Serial.print(GyZ);
+        Serial.print("||"); 
+        GyZ  = max(0.0,(GyZ-64400))/7800;
+        GyZ = GyZ ;
+        Serial.print(GyZ);
+        Serial.print("\n");
+      }
+      lastClockForG = crtClockForGyroscope;
 }
 
 #endif
