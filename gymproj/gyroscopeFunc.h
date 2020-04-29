4d99804770f4a704f3307b677eacb5379a6a8dcc
anson1788 <anson1788@gmail.com>
2020-04-29 17:58:07 +0800

    ad calibration logic

--- gymproj/gyroscopeFunc.h
+++ gymproj/gyroscopeFunc.h
@@ -11,21 +11,21 @@
 MPU6050 mpu;
 const int MPU_addr=0x68; 
 
-bool dmpReady = false;  // set true if DMP init was successful
+
 uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+Quaternion q;           
+VectorInt16 aa;         
+VectorInt16 aaReal;     
+VectorInt16 aaWorld;    
+VectorFloat gravity;    
+float euler[3];         
+float ypr[3];           
+
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
     mpuInterrupt = true;
@@ -49,29 +49,30 @@ void initI2Cdev(){
     mpu.setXGyroOffset(-73);
     mpu.setYGyroOffset(32);
     mpu.setZGyroOffset(-127);
+   
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
     
+    mpu.setXAccelOffset(mpu.AccelOffX); 
+    mpu.setYAccelOffset(mpu.AccelOffY); 
+    mpu.setZAccelOffset(mpu.AccelOffZ); 
+    mpu.setXGyroOffset(mpu.GyroOffX);
+    mpu.setYGyroOffset(mpu.GyroOffY);
+    mpu.setZGyroOffset(mpu.GyroOffZ);
+    
+     // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
 
-    // Calibration Time: generate offsets and calibrate our MPU6050
-        mpu.CalibrateAccel(6);
-        mpu.CalibrateGyro(6);
-        mpu.PrintActiveOffsets();
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-
-        // enable Arduino interrupt detection
-        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-        Serial.println(F(")..."));
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    packetSize = mpu.dmpGetFIFOPacketSize();
 
 }
 
@@ -91,7 +92,7 @@ void GetGyroscopeData(){
 
     
     crtClockForGyroscope = millis();
-    if (!dmpReady) return;
+
 
     // wait for MPU interrupt or extra packet(s) available
     while (!mpuInterrupt && fifoCount < packetSize) {
